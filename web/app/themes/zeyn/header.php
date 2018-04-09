169027fff648a225af3809d30e69acacb9e56454
Mathias Hedström <hedstrom.mathias@gmail.com>
2018-04-09 10:16:18 +0200

    Add gtag and Facebook pixel

--- web/app/themes/zeyn/header.php
+++ web/app/themes/zeyn/header.php
@@ -20,6 +20,53 @@ wp_head();
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-100067149-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-100067149-3');
+  gtag('config', 'UA-100067149-4');
+</script>
+    <!-- Facebook Pixel Code -->
+
+    <script>
+
+      !function(f,b,e,v,n,t,s)
+
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+
+        n.queue=[];t=b.createElement(e);t.async=!0;
+
+        t.src=v;s=b.getElementsByTagName(e)[0];
+
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+
+        'https://connect.facebook.net/en_US/fbevents.js');
+
+
+      fbq('init', '624108044602744');
+
+      fbq('track', 'PageView');
+
+    </script>
+
+    <noscript>
+
+        <img height="1" width="1"
+
+             src="https://www.facebook.com/tr?id=624108044602744&ev=PageView
+
+&noscript=1"/>
+
+    </noscript>
+
+    <!-- End Facebook Pixel Code -->
 </head>
 <body <?php body_class(is_detheme_home(get_post())?"home dt_custom_body":"dt_custom_body"); print $detheme_config['body_tag'];?>>
 <?php if($detheme_config['page_loader'] && !is_404()):?>
