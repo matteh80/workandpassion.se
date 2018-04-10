9936c28bec9d2dd65bd8a69351b2becdb1487a53
Mathias Hedström <hedstrom.mathias@gmail.com>
2018-04-10 13:50:46 +0200

    Add company template

---
+++ web/app/themes/waptheme/resources/views/template-company.blade.php
@@ -0,0 +1,20 @@
+{{--
+  Template Name: Company Page
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+    @include('partials.panel-candidate-how-it-works')
+
+    @while(have_posts()) @php(the_post())
+    @include('partials.content-page')
+    @endwhile
+
+    @include('partials.panel-candidate-contact')
+
+    <section class="sounds-fair d-flex flex-column align-items-center justify-content-center">
+        <h2 class="display-2 mb-5">Låter rättvist</h2>
+        <button class="btn btn-lg btn-outline-dark-gray">Skapa en profil</button>
+    </section>
+@endsection
