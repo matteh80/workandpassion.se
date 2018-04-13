be8c4ff276e464240be2caab9670b61c4e7eaf72
Mathias Hedström <hedstrom.mathias@gmail.com>
2018-04-13 11:57:14 +0200

    heading display-4

--- web/app/themes/waptheme/resources/views/layouts/panel.blade.php
+++ web/app/themes/waptheme/resources/views/layouts/panel.blade.php
@@ -1,7 +1,7 @@
 <section class="wap-panel bg-white {{ $panelclass }}">
     <div class="panel-image" style="background-image: url({{ $panelimage }})">
         <div class="overlay"></div>
-        <h2 class="panel-title display-3">{{ $paneltitle  }}</h2>
+        <h2 class="panel-title display-4">{{ $paneltitle  }}</h2>
     </div>
     <section class="panel-content py-5">
         @yield('panel-content')
